d39b4263be5e148fc3920e4501cbd6a69b39258f
PaperSlate <mhusain6@gatech.edu>
2018-02-14 14:16:05 -0500

    Added android project

---
+++ DoYouNodeDeWey/app/src/main/java/com/example/mohammed/doyounodedewey/LoggedIn.java
@@ -0,0 +1,30 @@
+package com.example.mohammed.doyounodedewey;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+public class LoggedIn extends AppCompatActivity {
+
+    public Button logoutBut;
+
+    public void init() {
+        logoutBut = findViewById(R.id.logoutBut);
+        logoutBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toy = new Intent(LoggedIn.this, LoginActivity.class);
+                startActivity(toy);
+            }
+            });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logged_in);
+        init();
+    }
+}
