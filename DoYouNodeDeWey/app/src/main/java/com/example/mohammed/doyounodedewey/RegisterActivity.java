c1c3289fa2f0a75e1d6ce4d369c742d10995b331
Chris Sun <csun84@gatech.edu>
2018-02-21 12:56:41 -0500

    Register page created, still need to add functionality

---
+++ DoYouNodeDeWey/app/src/main/java/com/example/mohammed/doyounodedewey/RegisterActivity.java
@@ -0,0 +1,41 @@
+package com.example.mohammed.doyounodedewey;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+        Button mRegisterCancelButton = (Button) findViewById(R.id.registerCancelButton);
+        mRegisterCancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+
+        Button mRegisterUserButton = (Button) findViewById(R.id.registerUserButton);
+        mRegisterUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+
+        Button mRegisterAdminButton = (Button) findViewById(R.id.registerAdminButton);
+        mRegisterAdminButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+    }
+}
