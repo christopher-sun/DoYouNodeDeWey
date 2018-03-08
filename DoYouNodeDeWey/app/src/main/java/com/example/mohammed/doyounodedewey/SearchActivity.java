13fba86b1e05f0cf46638674b7facbb439589660
unknown <derekwu120@gmail.com>
2018-03-08 01:00:19 -0500

    all the layout objects

--- DoYouNodeDeWey/app/src/main/java/com/example/mohammed/doyounodedewey/SearchActivity.java
+++ DoYouNodeDeWey/app/src/main/java/com/example/mohammed/doyounodedewey/SearchActivity.java
@@ -2,6 +2,9 @@ package com.example.mohammed.doyounodedewey;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 /**
  * Created by derek on 03/06/18.
@@ -9,9 +12,25 @@ import android.support.v7.app.AppCompatActivity;
 
 public class SearchActivity extends AppCompatActivity {
 
+    public EditText query;
+    public Button search;
+    public CheckBox female;
+    public CheckBox male;
+    public CheckBox families;
+    public CheckBox children;
+    public CheckBox youngAdults;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        query = findViewById(R.id.query);
+        search = findViewById(R.id.search);
+        female = findViewById(R.id.female);
+        male = findViewById(R.id.male);
+        families = findViewById(R.id.families);
+        children = findViewById(R.id.children);
+        youngAdults = findViewById(R.id.youngAdults);
     }
 }
